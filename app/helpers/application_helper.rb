dde43c4ac3047e490d1705f07c90b2eb09371f0c
harry <mrluonghuy@gmail.com>
2016-08-25 17:37:25 +0700

    hot fix sort posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
 	  title ||= column.titleize
 	  css_class = column == sort_column ? "current #{sort_direction}" : nil
 	  direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-	  if params[:category_id].present? || params[:fanpage_id].present?
-		  link_to title, {:date_from => params[:date_from], :date_end => params[:date_end], :category_id => params[:category_id], fanpage_id: params[:fanpage_id], :sort => column, :direction => direction}, {:class => css_class}
-	  else
-		  link_to title, {:sort => column, :direction => direction}, {:class => css_class}
-	  end
+		link_to title, {:date_from => params[:date_from], :date_end => params[:date_end], :category_id => params[:category_id], fanpage_id: params[:fanpage_id], :sort => column, :direction => direction}, {:class => css_class}
+	  # if params[:category_id].present? || params[:fanpage_id].present?
+	  # else
+		 #  link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+	  # end
 	end
 end
